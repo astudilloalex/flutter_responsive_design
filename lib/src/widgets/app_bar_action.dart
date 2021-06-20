335c66393caf67ff3795a226b8f3db08fecf0226
astudilloalex <astudilloalex1@gmail.com>
2021-06-20 15:33:54 -0500

    Add documentation to public variables

--- lib/src/widgets/app_bar_action.dart
+++ lib/src/widgets/app_bar_action.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 
 /// It must be used in a ResponsiveAppBar in actions.
 class AppBarAction extends StatelessWidget {
+  ///
   final Widget child;
+
+  /// Indicates if the action should be shown on medium screens.
+  /// Default is false
   final bool showInMediumScreen;
+
+  /// Indicates if the action should be shown in small screens.
+  /// Default is false
   final bool showInSmallScreen;
 
   AppBarAction({
     required this.child,
-    this.showInMediumScreen = true,
+    this.showInMediumScreen = false,
     this.showInSmallScreen = false,
   });
 
