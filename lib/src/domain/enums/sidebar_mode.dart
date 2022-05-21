6b924b84cf3c82f495672705adf8c58e3f7185ee
Alex Astudillo <astudilloalex1@gmail.com>
2022-05-21 18:20:27 -0500

    Add the sidebar mode

---
+++ lib/src/domain/enums/sidebar_mode.dart
@@ -0,0 +1,11 @@
+/// Mode to manage sidebar modes.
+enum SidebarMode {
+  /// Change between [compact] and [open] depending of the screen size.
+  auto,
+
+  /// The sidebar show only icons.
+  compact,
+
+  /// The sidebar is expanded show text and icon.
+  open,
+}
